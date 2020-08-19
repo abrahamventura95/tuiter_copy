48190051548dfccf05a3fb14c7b6fea4f29315fb
Abraham Ventura <amsventura.95@gmail.com>
2020-08-19 13:04:26 -0400

    Like
    Like and unlike a tuit

---
+++ Controllers/like.js
@@ -0,0 +1,23 @@
+var validator	= require('validator');
+var functions	= require('./functions');
+var queries		= require('../DB/Connections/like');
+
+exports.create = function(req, res) {
+	var like = {
+		idUser: 	req.user.sub,
+		idTuit: 	req.body.idTuit
+	};				
+	queries.create(like, function(err, data){
+		res.json(data);
+	});
+};
+
+exports.delete = function(req, res){
+	var like = {
+		idUser: 	req.user.sub,
+		id: req.param('id')
+	}
+	queries.delete(like, function(err, data){
+		res.json(data);
+	});
+}
--- routes/tuits.js
+++ routes/tuits.js
@@ -1,8 +1,12 @@
-var controller = require('../Controllers/tuit');
-var auth = require('../Middleware/auth');
+var auth 		= require('../Middleware/auth');
+var like 	   	= require('../Controllers/like');
+var controller 	= require('../Controllers/tuit');
 
 module.exports = function(app) {	
 	app.route('/tuit')
 	  	.post(auth.check, controller.create)
 		.delete(auth.check, controller.delete);
+	app.route('/tuit/like')
+		.post(auth.check, like.create)
+		.delete(auth.check, like.delete);
 };
